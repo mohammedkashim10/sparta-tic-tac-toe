ce8621574e859a48d34bdb9a78528e0a11472387
Tech-A36 <tech-a36@tech-a36s-air.home>
2018-09-13 22:57:00 +0100

    alternating X and O, button resets, header changes, but no win condition

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# sparta-tic-tac-toe
+# Tic Tac Toe
+### Description
+Produce a working game of tic tac toe given a three-by-three grid template
+
+### Languages Used
+* JavaScript
+
+The grid was already made with HTML with CSS styling but these were not to be altered in any way.
+
+### How to Download
+1. Link to the GitHub repository: https://github.com/mohammedkashim10/sparta-tic-tac-toe
+2. Click on 'Clone or download' and copy the SSH key (git@github.com:mohammedkashim10/sparta-tic-tac-toe.git)
+3. In your terminal, in your desired location, carry out the command 'git clone -SSH key-'
+
+### Challenges
+It was difficult at first to alternate between clicks since this is a two player game; alternating between X and O. However, this was overcome using booleans - using 'if' statements and changing the value of the boolean allowed me to change the outcome of the click depending on the value. Also, I did not manage to generate a win condition, so the game continues without alerting who won the game. I tried using arrays, specifically the 'indexOf()' function but I was missing something which prevented it from working.
+
+### Learning Points
+I'm getting more comfortable with object oriented programming and manipulating the DOM, for example creating and calling elements, assigning classes etc. using just JavaScript.
--- script.js
+++ script.js
@@ -4,37 +4,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
   Game.square = document.getElementsByTagName('td');
   Game.player = document.getElementsByTagName('h2');
+  Game.reset = document.getElementById('reset');
 
-  let playerTurn = true;
+  Game.playerTurn = true;
 
   Game.winConditions = [
     [0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]
   ]
 
-  const arrX = [];
-  const arrO = [];
+  Game.arrX = [];
+  Game.arrO = [];
+
+  Game.reset.addEventListener('click', (e) => {
+    location.reload();
+  })
 
   for (let i=0; i<Game.square.length; i++) {
     Game.square[i].addEventListener('click', (e) => {
-      switch (playerTurn) {
+      switch (Game.playerTurn) {
         case true:
           Game.square[i].setAttribute('class', 'X');
           Game.square[i].innerHTML = 'X';
-          arrX.push(Game.square[i].attributes[0].value);
-          console.log(arrX);
-          // Game.player.innerHTML = 'It is O\'s turn'; // doesn't work at the moment
-          playerTurn = false;
+          Game.arrX.push(Number(Game.square[i].attributes[0].value));
+          console.log(Game.arrX);
+          Game.player[0].innerHTML = 'It is O\'s turn';
+          Game.playerTurn = false;
           break;
         case false:
           Game.square[i].setAttribute('class', 'O');
           Game.square[i].innerHTML ='O';
-          arrO.push(Game.square[i].attributes[0].value);
-          console.log(arrO);
-          // Game.player.innerHTML = 'It is X\'s turn'; // doesn't work at the moment
-          playerTurn = true;
+          Game.arrO.push(Number(Game.square[i].attributes[0].value));
+          console.log(Game.arrO);
+          Game.player[0].innerHTML = 'It is X\'s turn';
+          Game.playerTurn = true;
           break;
       }
-      // if Game.square[i] has a class, stop this function
     })
   }
 });
