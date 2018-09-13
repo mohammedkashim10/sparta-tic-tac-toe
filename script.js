fb5cccca2c6c0ff36b9670fe404e7750cc43089d
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-09-13 16:17:54 +0100

    need to do win condition

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+
+  const Game = new Object();
+
+  Game.square = document.getElementsByTagName('td');
+  Game.player = document.getElementsByTagName('h2');
+
+  let playerTurn = true;
+
+  Game.winConditions = [
+    [0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]
+  ]
+
+  const arrX = [];
+  const arrO = [];
+
+  for (let i=0; i<Game.square.length; i++) {
+    Game.square[i].addEventListener('click', (e) => {
+      switch (playerTurn) {
+        case true:
+          Game.square[i].setAttribute('class', 'X');
+          Game.square[i].innerHTML = 'X';
+          arrX.push(Game.square[i].attributes[0].value);
+          console.log(arrX);
+          // Game.player.innerHTML = 'It is O\'s turn'; // doesn't work at the moment
+          playerTurn = false;
+          break;
+        case false:
+          Game.square[i].setAttribute('class', 'O');
+          Game.square[i].innerHTML ='O';
+          arrO.push(Game.square[i].attributes[0].value);
+          console.log(arrO);
+          // Game.player.innerHTML = 'It is X\'s turn'; // doesn't work at the moment
+          playerTurn = true;
+          break;
+      }
+      // if Game.square[i] has a class, stop this function
+    })
+  }
 });
